c9560c86f029476c94cd336aec07f98bd9085452
MARTINA1102 <martinamiccoli11@gmail.com>
2023-01-18 09:41:00 +0100

    completo

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,13 +8,9 @@ use Illuminate\Http\Request;
 class HomeController extends Controller
 {
     public function home() {
-        return view('guest.home');
-    }
-
-    public function list() {
-
         // SELECT * FROM `trains`
         $trains = Trains::all();
         return view('guest.home', compact('trains'));
     }
+
 }
